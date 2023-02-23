88c3ab93f6506d6f59942d635e441b37735238b4
ywvv <ywvvvw@gmail.com>
2023-02-23 17:48:32 +0200

    feat: add layout

--- app/src/main/java/com/example/countriesapp/MainActivity.kt
+++ app/src/main/java/com/example/countriesapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.countriesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.countriesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
